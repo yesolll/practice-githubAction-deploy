116ee7d02240e66c28f30239c4447f261f61b35b
yesolll <9595kys@gmail.com>
2023-02-06 13:05:57 +0900

    first commit

---
+++ src/main/java/com/codestates/practicegithunActiondeploy/PracticeGithunActionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithunActiondeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithunActionDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithunActionDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithunActiondeploy/PracticeGithunActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithunActiondeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithunActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
